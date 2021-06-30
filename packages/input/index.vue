7844177911f0dccdb46868cbc4f1d24c2f62f43a
liyuelong <1393926171@qq.com>
2021-06-30 17:15:11 +0800

    input事件触发修改

--- packages/input/index.vue
+++ packages/input/index.vue
@@ -22,14 +22,7 @@
       <div class="pl-input-value">
         <template v-if="type !== 'textarea'">
           <div class="pl-input-inner">
-            <input
-              v-bind="$attrs"
-              v-on="{input: emit, focus: emit, blur: emit}"
-              v-if="type !== 'textarea'"
-              :type="type"
-              :disabled="calcDisabled"
-              :value="currentValue"
-              ref="input">
+            <input v-bind="$attrs" v-on="{input: emit, focus: emit, blur: emit, change: 'emit'}" v-if="type !== 'textarea'" :type="type" :disabled="calcDisabled" :value="currentValue" ref="input">
           </div>
           <div class="pl-input-clear" @touchstart="clear" @mousedown="clear" v-show="showClear">
             <icon name="icon-roundclosefill" fill="#ccc"></icon>
@@ -39,14 +32,7 @@
           </div>
         </template>
         <div class="pl-input-inner" v-else>
-        <textarea
-          v-bind="$attrs"
-          :rows="rows"
-          :cols="cols"
-          v-on="{input: emit, focus: emit, blur: emit}"
-          :value="currentValue"
-          ref="input"
-          :disabled="calcDisabled"></textarea>
+          <textarea v-bind="$attrs" :rows="rows" :cols="cols" v-on="{input: emit, focus: emit, blur: emit, change: 'emit'}" :value="currentValue" ref="input" :disabled="calcDisabled"></textarea>
         </div>
       </div>
 
@@ -56,306 +42,304 @@
 </template>
 
 <script>
-  import icon from '../icon/index.vue'
-  import {validate} from '../../src/assets/utils'
+import icon from '../icon/index.vue'
+import { validate } from '../../src/assets/utils'
 
-  export default {
-    name: 'plInput',
-    componentName: 'plInput',
-    components: {
-      icon
-    },
-    props: {
-      rules: {          // 验证规则
-        type: Array,
-        default () {
-          return []
-        }
-      },
-      size: String,     // 尺寸 可选值为 normal，large, small,
-      type: {         // 表单类型，原生字段
-        type: String,
-        default: 'text'
-      },
-      value: [String, Number],
-      wrap: Boolean,            // label与value换行显示
-      disabled: Boolean,        // 禁用
-      required: Boolean,        // 必填 *号
-      rows: String,            // textarea rows
-      cols: String,            // textarea cols
-      label: String,            // 左侧 label
-      labelWidth: String,       // label 宽度
-      clearable: {              // 清除按钮
-        type: Boolean,
-        default: false
+export default {
+  name: 'plInput',
+  componentName: 'plInput',
+  components: {
+    icon
+  },
+  props: {
+    rules: {          // 验证规则
+      type: Array,
+      default() {
+        return []
       }
     },
-    inject: {
-      form: {
-        default: null
-      }
+    size: String,     // 尺寸 可选值为 normal，large, small,
+    type: {         // 表单类型，原生字段
+      type: String,
+      default: 'text'
     },
-    data () {
-      return {
-        currentValue: this.value === undefined ? '' : this.value,
-        focused: false,
-        ruleMessage: '',     // 验证错误提示信息
-        handlers: []
-      }
+    value: [String, Number],
+    wrap: Boolean,            // label与value换行显示
+    disabled: Boolean,        // 禁用
+    required: Boolean,        // 必填 *号
+    rows: String,            // textarea rows
+    cols: String,            // textarea cols
+    label: String,            // 左侧 label
+    labelWidth: String,       // label 宽度
+    clearable: {              // 清除按钮
+      type: Boolean,
+      default: false
+    }
+  },
+  inject: {
+    form: {
+      default: null
+    }
+  },
+  data() {
+    return {
+      currentValue: this.value === undefined ? '' : this.value,
+      focused: false,
+      ruleMessage: '',     // 验证错误提示信息
+      handlers: []
+    }
+  },
+  computed: {
+    showClear() {
+      return this.clearable && !this.calcDisabled && this.currentValue !== '' && this.focused
     },
-    computed: {
-      showClear () {
-        return this.clearable && !this.calcDisabled && this.currentValue !== '' && this.focused
-      },
-      calcSize () {
-        return this.size || this.form && this.form.size || 'normal'
-      },
-      calcLabelWidth () {
-        return this.labelWidth || this.form && this.form.labelWidth || null
-      },
-      calcDisabled () {
-        return this.disabled !== undefined ? this.disabled : this.form && this.form.disabled !== undefined ? this.form.disabled : false
-      }
+    calcSize() {
+      return this.size || this.form && this.form.size || 'normal'
     },
-    mounted () {
-      this.bindEvent()
-      if (this.form) {
-        this.form.updateItems(this);
-      }
+    calcLabelWidth() {
+      return this.labelWidth || this.form && this.form.labelWidth || null
     },
-    methods: {
-      // 手动验证方法
-      validate () {
-        return Promise.all(this.rules.map(rule => validate(rule, this.value))).then(() => {
-          this.ruleMessage = ''
-          return Promise.resolve()
-        }).catch(e => {
-          this.ruleMessage = e
-          return Promise.reject(e)
-        })
-      },
-      clearValidate () {
+    calcDisabled() {
+      return this.disabled !== undefined ? this.disabled : this.form && this.form.disabled !== undefined ? this.form.disabled : false
+    }
+  },
+  mounted() {
+    this.bindEvent()
+    if (this.form) {
+      this.form.updateItems(this);
+    }
+  },
+  methods: {
+    // 手动验证方法
+    validate() {
+      return Promise.all(this.rules.map(rule => validate(rule, this.value))).then(() => {
         this.ruleMessage = ''
-      },
-      focus () {
-        this.$refs.input.focus();
-      },
-      blur () {
-        this.$refs.input.blur();
-      },
-      setCurrentValue (value) {
-        if (value === this.currentValue) {
-          return false
-        }
-        this.currentValue = value
-      },
-      clear () {
-        this.$emit('input', '')
-        this.$emit('change', '')
-        this.$emit('clear')
-        this.setCurrentValue('')
-        this.focus()
-      },
-      emit (e) {
-        const value = e.target.value
-        const type = e.type
-
-        if (type === 'input') {
-          this.setCurrentValue(value)
-        }
-        if (type === 'focus') {
-          this.focused = true
-        }
-        if (type === 'blur') {
-          this.focused = false
-        }
-        this.$emit(type, value)
-      },
-      bindEvent () {
-        // 绑定事件
-        this.rules.forEach(rule => {
-          let event = rule.trigger
-          if (!event) {
-            event = 'change'
-          }
-
-          let handler = () => {
-            validate(rule, this.value).then(() => {
-              this.ruleMessage = ''
-            }).catch(e => {
-              this.ruleMessage = e
-            })
-          }
-
-          this.$refs.input.addEventListener(event, handler)
-          this.handlers.push({event, handler})
-        })
-      },
-      unbindEvent () {
-        this.handlers.forEach(handler => {
-          this.$refs.input.removeEventListener(handler.event, handler.handler)
-        })
-        this.handlers.length = 0
-      },
+        return Promise.resolve()
+      }).catch(e => {
+        this.ruleMessage = e
+        return Promise.reject(e)
+      })
     },
-    watch: {
-      'value' (val) {
-        this.setCurrentValue(val)
-      },
-      'rules' (val) {
-        if (Array.isArray(val)) {
-          this.unbindEvent()
-          this.bindEvent()
-        }
+    clearValidate() {
+      this.ruleMessage = ''
+    },
+    focus() {
+      this.$refs.input.focus();
+    },
+    blur() {
+      this.$refs.input.blur();
+    },
+    setCurrentValue(value) {
+      if (value === this.currentValue) {
+        return false
       }
+      this.currentValue = value
     },
-    beforeDestroy () {
-      this.unbindEvent()
+    clear() {
+      this.$emit('input', '')
+      this.$emit('change', '')
+      this.$emit('clear')
+      this.setCurrentValue('')
+      this.focus()
     },
-    destroyed () {
-      if (this.$el && this.$el.parentNode) {
-        this.$el.parentNode.removeChild(this.$el);
+    emit(e) {
+      const value = e.target.value
+      const type = e.type
+
+      if (type === 'input') {
+        this.setCurrentValue(value)
       }
-      if (this.form) {
-        this.form.removeItem(this);
+      if (type === 'focus') {
+        this.focused = true
+      }
+      if (type === 'blur') {
+        this.focused = false
+      }
+      this.$emit(type, value)
+    },
+    bindEvent() {
+      // 绑定事件
+      this.rules.forEach(rule => {
+        let event = rule.trigger
+        if (!event) {
+          event = 'change'
+        }
+
+        let handler = () => {
+          validate(rule, this.value).then(() => {
+            this.ruleMessage = ''
+          }).catch(e => {
+            this.ruleMessage = e
+          })
+        }
+
+        this.$refs.input.addEventListener(event, handler)
+        this.handlers.push({ event, handler })
+      })
+    },
+    unbindEvent() {
+      this.handlers.forEach(handler => {
+        this.$refs.input.removeEventListener(handler.event, handler.handler)
+      })
+      this.handlers.length = 0
+    },
+  },
+  watch: {
+    'value'(val) {
+      this.setCurrentValue(val)
+    },
+    'rules'(val) {
+      if (Array.isArray(val)) {
+        this.unbindEvent()
+        this.bindEvent()
       }
     }
+  },
+  beforeDestroy() {
+    this.unbindEvent()
+  },
+  destroyed() {
+    if (this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el);
+    }
+    if (this.form) {
+      this.form.removeItem(this);
+    }
   }
+}
 </script>
 
 <style lang="less" scoped>
+.pl-input {
+  background-color: var(--input-bg);
+  padding: 0 1.2rem;
+  line-height: normal;
 
-  .pl-input {
-    background-color: var(--input-bg);
-    padding: 0 1.2rem;
-    line-height: normal;
+  * {
+    box-sizing: border-box;
+  }
 
-    * {
-      box-sizing: border-box;
-    }
-
-    &-cell {
-      display: flex;
-      flex-wrap: nowrap;
-      align-items: center;
-      &--label {
-        .pl-input-title {
-          padding: 1em 0;
-        }
-        .pl-input-inner {
-          input,
-          textarea {
-            text-align: right;
-          }
-        }
+  &-cell {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    &--label {
+      .pl-input-title {
+        padding: 1em 0;
       }
-      &--wrap {
-        flex-direction: column;
-
-        .pl-input-title,
-        .pl-input-value {
-          width: 100%;
-        }
-        .pl-input-title {
-          padding-top: 1em;
+      .pl-input-inner {
+        input,
+        textarea {
+          text-align: right;
         }
       }
     }
-    &-title,
-    &-value {
-      display: flex;
-      flex-wrap: nowrap;
-      align-items: center;
-      flex-direction: row;
-    }
-    &-title {
-      &--start {
-        align-self: flex-start;
+    &--wrap {
+      flex-direction: column;
+
+      .pl-input-title,
+      .pl-input-value {
+        width: 100%;
       }
-      &--require {
-        &::before {
-          display: inline-block;
-          width: 0.6rem;
-          content: '*';
-          color: var(--danger);
-          margin-left: -0.6rem;
-        }
+      .pl-input-title {
+        padding-top: 1em;
       }
-    }
-    &-value {
-      flex: 1;
-    }
-
-    &--large {
-      font-size: 1.2em;
-    }
-    &--small {
-      font-size: 0.8em;
-    }
-    &--normal {
-
-    }
-    &--error {
-      position: relative;
-    }
-    &-inner {
-      padding: 1em 0;
-      flex: 1;
-    }
-    .pl-input-label {
-      padding-right: 0.4em;
-    }
-    .pl-textarea-label {
-      align-self: flex-start;
-    }
-    &-append,
-    &-prepend {
-      text-align: center;
-    }
-    &-append {
-      padding-left: 0.4em;
-    }
-    &-prepend {
-      padding-right: 0.4em;
-    }
-    &-clear {
-      margin-left: 0.4em;
-
-      .icon-clear {
-        width: 1.2em;
-        height: 1.2em;
-        vertical-align: bottom;
-      }
-    }
-    &-error {
-      padding: 0 0.5em;
-      color: var(--danger);
-      line-height: 2em;
-    }
-
-    input,
-    textarea {
-      border: 0 none;
-      margin: 0;
-      padding: 0;
-      line-height: normal;
-      width: 100%;
-      background-color: transparent;
-      font-size: 1em;
-      vertical-align: middle;
-
-      &::placeholder {
-        color: var(--primary-text);
-      }
-      &:active,
-      &:focus,
-      &:hover {
-        outline: 0 none;
-      }
-    }
-
-    &.is-disabled {
-      background-color: var(--disabled);
     }
   }
+  &-title,
+  &-value {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    flex-direction: row;
+  }
+  &-title {
+    &--start {
+      align-self: flex-start;
+    }
+    &--require {
+      &::before {
+        display: inline-block;
+        width: 0.6rem;
+        content: "*";
+        color: var(--danger);
+        margin-left: -0.6rem;
+      }
+    }
+  }
+  &-value {
+    flex: 1;
+  }
+
+  &--large {
+    font-size: 1.2em;
+  }
+  &--small {
+    font-size: 0.8em;
+  }
+  &--normal {
+  }
+  &--error {
+    position: relative;
+  }
+  &-inner {
+    padding: 1em 0;
+    flex: 1;
+  }
+  .pl-input-label {
+    padding-right: 0.4em;
+  }
+  .pl-textarea-label {
+    align-self: flex-start;
+  }
+  &-append,
+  &-prepend {
+    text-align: center;
+  }
+  &-append {
+    padding-left: 0.4em;
+  }
+  &-prepend {
+    padding-right: 0.4em;
+  }
+  &-clear {
+    margin-left: 0.4em;
+
+    .icon-clear {
+      width: 1.2em;
+      height: 1.2em;
+      vertical-align: bottom;
+    }
+  }
+  &-error {
+    padding: 0 0.5em;
+    color: var(--danger);
+    line-height: 2em;
+  }
+
+  input,
+  textarea {
+    border: 0 none;
+    margin: 0;
+    padding: 0;
+    line-height: normal;
+    width: 100%;
+    background-color: transparent;
+    font-size: 1em;
+    vertical-align: middle;
+
+    &::placeholder {
+      color: var(--primary-text);
+    }
+    &:active,
+    &:focus,
+    &:hover {
+      outline: 0 none;
+    }
+  }
+
+  &.is-disabled {
+    background-color: var(--disabled);
+  }
+}
 </style>
