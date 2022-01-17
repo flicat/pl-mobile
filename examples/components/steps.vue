fb4ed0120d70cff2dcae8edc3481528cb84a8db2
Flicat.li <flicat.li@qq.com>
2022-01-17 17:32:15 +0800

    steps组件添加收起按钮控制

--- examples/components/steps.vue
+++ examples/components/steps.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="content">
-    <pl-steps :active="0">
-      <pl-step-item>步骤一</pl-step-item>
-      <pl-step-item>步骤二</pl-step-item>
-      <pl-step-item>步骤三</pl-step-item>
+    <pl-steps :active="0" showFold>
+      <pl-step-item>
+        <h4>步骤一的标题</h4>
+        <p>步骤一的描述</p>
+      </pl-step-item>
+      <pl-step-item>
+        <h4>步骤二的标题</h4>
+        <p>步骤二的描述</p>
+      </pl-step-item>
+      <pl-step-item>
+        <h4>步骤三的标题</h4>
+        <p>步骤三的描述</p>
+      </pl-step-item>
     </pl-steps>
-
+    <hr>
     <pl-steps :active="0" direction="horizontal">
       <pl-step-item>步骤一</pl-step-item>
       <pl-step-item>步骤二</pl-step-item>
@@ -19,5 +28,4 @@
 </script>
 
 <style lang="less" scoped>
-
 </style>
